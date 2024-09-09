45366369c95e6c45eeb58c859afa5c3726210d0e
Melani <coraline2719@icloud.com>
2024-09-09 15:04:46 +0200

    JVM arguments for each startup script
    Also some README.md changes

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-# MithrilMC/Scripts
+# GnarkMC/Scripts
 
-> This repository is a mnemonic to store several scripts that can go from about any kind of use related to Minecraft.
+> This repository is a mnemonic to store several scripts that can go from about any kind of use related to Minecraft & Linux.
+
+[`.bashrc`](https://github.com/GnarkMC/Scripts/blob/main/.bashrc) |
+[`start-proxy.sh`](https://github.com/GnarkMC/Scripts/blob/main/start-proxy.sh) |
+[`start-server.sh`](https://github.com/GnarkMC/Scripts/blob/main/start-server.sh) |
 
 # Scripts:
 
-## start.sh
-Universal start script for Purpur & Velocity.
+> A explanation of each file:
 
 ## .bashrc
-.bashrc config for Linux.
+Bash file for Linux, currently offers:
+- Automatically set JAVA upon login from a specified path.
+- Auto navigate to the users directory.
+
+## start-proxy.sh & start-server.sh
+Startup scripts for a Velocity instance and a Minecraft server.
+- Smart JAR, automatically finds the latest version
+- Easy to configure, all changeable keys are at the top.
--- start-proxy.sh
+++ start-proxy.sh
@@ -1,50 +1,26 @@
-# https://github.com/MithrilMC/Scripts/blob/main/start.sh
+# https://github.com/GnarkMC/Scripts/blob/main/start-proxy.sh
 
-# Search for the latest Purpur or Velocity JAR file
-latest_purpur=$(ls purpur-1.21.1-*.jar | sort -V | tail -n 1)
-latest_velocity=$(ls velocity-3.3.0-SNAPSHOT-*.jar | sort -V | tail -n 1)
+# User-configurable options
+BASE_NAME="velocity-3.3.0-SNAPSHOT-"    # Set the base name of the JAR file (before the version number)
+HEAP_SIZE=4096                         # Set heap size for the server (megabytes)
+JAVA_OPTS=(                            # Java command-line options
+    "-Xms${HEAP_SIZE}M"                # Initial heap size
+    "-Xmx${HEAP_SIZE}M"                # Maximum heap size
+    "-XX:+UseG1GC"                     # Use G1 Garbage Collector
+    "-XX:G1HeapRegionSize=4M"          # Set G1 heap region size
+    "-XX:+UnlockExperimentalVMOptions" # Unlock experimental VM options
+    "-XX:+ParallelRefProcEnabled"      # Enable parallel reference processing
+    "-XX:+AlwaysPreTouch"              # Pre-touch memory to improve performance
+    "-XX:MaxInlineLevel=15"            # Increase the maximum inlining level
+)
 
-# Check if either JAR file exists
-if [ -n "$latest_purpur" ]; then
-    jar_type="Purpur"
-    latest_jar="$latest_purpur"
+# Find the Purpur JAR file with the highest version number
+NAME=$(ls ${BASE_NAME}*.jar 2>/dev/null | sort -V | tail -n 1)
 
-    # Set heap size for Purpur in megabytes.
-    heap_size=16384
-
-    # Java command-line options for Purpur
-    java_opts=(
-        "-Xms${heap_size}M"                # Initial heap size for Purpur
-        "-Xmx${heap_size}M"                # Maximum heap size for Purpur
-    )
-
-    echo "Using $jar_type JAR file: $latest_jar"
-    java "${java_opts[@]}" -jar "$latest_jar" --nogui
-
-elif [ -n "$latest_velocity" ]; then
-    jar_type="Velocity"
-    latest_jar="$latest_velocity"
-
-    # Set heap size for Velocity in megabytes.
-    heap_size=4096
-
-    # Java command-line options for Velocity
-    java_opts=(
-        "-Xms${heap_size}M"                # Initial heap size for Velocity
-        "-Xmx${heap_size}M"                # Maximum heap size for Velocity
-        "-XX:+UseG1GC"                     # Use G1 Garbage Collector
-        "-XX:G1HeapRegionSize=4M"          # Set G1 heap region size
-        "-XX:+UnlockExperimentalVMOptions" # Unlock experimental VM options
-        "-XX:+ParallelRefProcEnabled"      # Enable parallel reference processing
-        "-XX:+AlwaysPreTouch"              # Pre-touch memory to improve performance
-        "-XX:MaxInlineLevel=15"            # Increase the maximum inlining level
-    )
-
-    echo "Using $jar_type JAR file: $latest_jar"
-    java "${java_opts[@]}" -jar "$latest_jar"
-
-else
-    # Exit if no relevant JAR file is found
+if [ -n "$NAME" ]; then # Check if any JAR file was found
+    echo "Using JAR file: $NAME"
+    java "${JAVA_OPTS[@]}" -jar "$NAME"
+else # Exit if no JAR file is found
     echo "No relevant JAR file found."
     exit 1
 fi
